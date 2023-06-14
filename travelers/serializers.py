818494da22bf0e90ac13df7bca8096a334fdd9eb
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-14 13:56:40 +0000

    Add IsOwner permissions

--- travelers/serializers.py
+++ travelers/serializers.py
@@ -5,10 +5,15 @@ from .models import Traveler
 class TravelerSerializer(serializers.ModelSerializer):
     # Serializer class for the traveler model
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Traveler
         fields = ['id', 'owner', 'created_at',
                   'updated_at', 'name', 'content', 'image',
                   'top_bucket_list', 'one_important_thing',
-                  'favorite_place']
+                  'favorite_place', 'is_owner']
--- travelers/views.py
+++ travelers/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Traveler
 from .serializers import TravelerSerializer
 from django.http import Http404
+from backend.permissions import IsOwnerOrReadOnly
 
 
 class TravelerList(APIView):
@@ -11,19 +12,24 @@ class TravelerList(APIView):
     List all travelers
     No Create view (post method), as profile creation handled by django signals
     """
+
     def get(self, request):
         travelers = Traveler.objects.all()
-        serializer = TravelerSerializer(travelers, many=True)
+        serializer = TravelerSerializer(
+            travelers, many=True, context={'request': request})
         return Response(serializer.data)
 
 
 class TravelerDetail(APIView):
     serializer_class = TravelerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         # Retrive a specific traverel based on the primary key
         try:
             traveler = Traveler.objects.get(pk=pk)
+            # Check permissions to ensure only the owner can modify their traveler profile.
+            self.check_object_permissions(self.request, traveler)
             return traveler
         except Traveler.DoesNotExist:
             raise Http404
@@ -31,15 +37,14 @@ class TravelerDetail(APIView):
     def get(self, request, pk):
         # Handle GET request to retrieve details of a traveler
         traveler = self.get_object(pk)
-        serializer = TravelerSerializer(traveler)
+        serializer = TravelerSerializer(traveler, context={'request': request})
         return Response(serializer.data)
 
     def put(self, request, pk):
         # Handle PUT request to update details of a traveler
         traveler = self.get_object(pk)
-        serializer = TravelerSerializer(traveler, data=request.data)
+        serializer = TravelerSerializer(traveler, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
