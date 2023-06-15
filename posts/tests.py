477ac72415b843b5142cdf02c9ea35d8703a228a
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-15 10:17:09 +0000

    Add tests to posts

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='testUser', password='pass')
+
+    def test_can_list_posts(self):
+        testUser = User.objects.get(username='testUser')
+        Post.objects.create(owner=testUser, title='test title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='testUser', password='pass')
+        response = self.client.post('/posts/', {'title': 'test title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_not_logged_in_cant_create_post(self):
+        response = self.client.post('/posts/', {'title': 'test title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_can_retrieve_specific_post(self):
+        testUser = User.objects.get(username='testUser')
+        post = Post.objects.create(owner=testUser, title='test title')
+        response = self.client.get(f'/posts/{post.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'test title')
