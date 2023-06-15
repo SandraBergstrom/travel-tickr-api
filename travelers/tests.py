19a7f0ad2f31ab0c22b86d62310b1997d8267511
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-15 10:41:04 +0000

    Add test to traveler

--- travelers/tests.py
+++ travelers/tests.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
+from .models import Traveler
 from django.test import TestCase
 
-# Create your tests here.
+
+class UserTravelerCountTest(TestCase):
+    # make sure that the user is always extended with the traveler model
+    def test_user_traveler_count(self):
+        user_count = User.objects.count()
+        traveler_count = Traveler.objects.count()
+        self.assertEqual(user_count, traveler_count)
