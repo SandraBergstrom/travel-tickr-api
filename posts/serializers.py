99da33627bde00d89e30cdabda16823c4169593e
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-15 08:58:35 +0000

    Add postlistview and serializer to posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from posts.models import Post
-from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -8,9 +7,6 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    like_id = serializers.SerializerMethodField()
-    likes_count = serializers.ReadOnlyField()
-    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -29,20 +25,10 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
-    def get_like_id(self, obj):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            like = Like.objects.filter(
-                owner=user, post=obj
-            ).first()
-            return like.id if like else None
-        return None
-
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'like_id',
-            'likes_count', 'comments_count'
+            'title', 'content', 'image',
         ]
--- posts/views.py
+++ posts/views.py
@@ -12,3 +12,17 @@ class PostList(APIView):
             posts, many=True, context={'request': request}
         )
         return Response(serializer.data)
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostList(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context=('request': request)
+        )
+        return Response(serializer.data)
