0f564f309f06f2f71ec6f064067b884e43c20a40
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-21 09:11:35 +0000

    #1 Correct CORS settings

--- backend/settings.py
+++ backend/settings.py
@@ -110,15 +110,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# if 'CLIENT_ORIGIN' in os.environ:
-#     CORS_ALLOWED_ORIGINS = [
-#         os.environ.get('CLIENT_ORIGIN')
-#     ]
-# else:
-#     CORS_ALLOWED_ORIGIN_REGEXES = [
-#         r"^https://.*\.gitpod\.io$",
-#     ]
-
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN'),
