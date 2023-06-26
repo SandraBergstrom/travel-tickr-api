223a22479350ea13e4bd0a03a3b8637337e6877b
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-26 19:44:19 +0000

    #15 correct filterset fields

--- posts/views.py
+++ posts/views.py
@@ -25,9 +25,9 @@ class PostList(generics.ListCreateAPIView):
     ]
     
     filterset_fields = [
-        'owner__followed__owner__profile',
-        'likes__owner__profile',
-        'owner__profile',
+        'owner__followed__owner__traveler',
+        'likes__owner__traveler',
+        'owner__traveler',
     ]
     search_fields = [
         'owner__username',
