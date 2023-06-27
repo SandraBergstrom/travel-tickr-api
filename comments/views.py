997be21c3da8ff2f3729d9deb8bc7d0a1bb2f6ea
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-27 08:19:59 +0000

    Correct typo

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
-from backend_api.permissions import IsOwnerOrReadOnly
+from backend.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
