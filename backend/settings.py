406dac2f267a9580c911f44626a8aeac1b847266
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-27 07:21:19 +0000

    Correct typo

--- backend/settings.py
+++ backend/settings.py
@@ -51,7 +51,7 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'backend.serializers.CurrentUserSerializer'
 }
 
 # Quick-start development settings - unsuitable for production
