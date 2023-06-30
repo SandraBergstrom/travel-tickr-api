5a7ca62f2bffdc1b50f55104ee2d816d5c039172
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-30 10:06:47 +0000

    #36 Change location model fields owner and post

--- locations/models.py
+++ locations/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
 
+
 class Location(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     post = models.ForeignKey(
-        Post, related_name='location', on_delete=models.CASCADE
+        Post, related_name='location', on_delete=models.CASCADE, null=True, blank=True
     )
     name = models.CharField(max_length=150)
 
