5a281a88c743f7d606bdf3b8db96fb38d08aa0d7
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-30 07:08:04 +0000

    #36 Add views to location

--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from backend.permissions import IsOwnerOrReadOnly
+from .models import Location
+from .serializers import LocationSerializer
 
-# Create your views here.
+
+class LocationList(generics.ListCreateAPIView):
+    """
+    List all locations.
+    Create a location if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
