f82e2740b6994ac53a74de47a7efd739f2360385
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-07-01 12:44:14 +0000

    #36 Add country field to location model

--- locations/models.py
+++ locations/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Location(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
+    country = models.CharField(max_length=150, default="Unknown")
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.country}"
