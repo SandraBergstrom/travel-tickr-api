1f5f731c299406166a492968ff98a4126294ee07
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-07-03 17:02:57 +0000

    #54 Add like_id, like_count to comment serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -11,13 +11,25 @@ class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     traveler_id = serializers.ReadOnlyField(source='owner.traveler.id')
-    traveler_image = serializers.ReadOnlyField(source='owner.traveler.image.url')
+    traveler_image = serializers.ReadOnlyField(
+        source='owner.traveler.image.url')
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, comment=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     def get_created_at(self, obj):
         return naturaltime(obj.created_at)
@@ -29,7 +41,7 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'traveler_id', 'traveler_image',
-            'post', 'created_at', 'updated_at', 'content'
+            'post', 'created_at', 'updated_at', 'content', 'likes_count', 'like_id'
         ]
 
 
