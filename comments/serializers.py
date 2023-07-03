1bde5bc6136fc92fdb32b2f6bee1ef11498c1750
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-07-03 18:41:04 +0000

    # 54 Import likes model

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
+from likes.models import Like
 
 
 class CommentSerializer(serializers.ModelSerializer):
